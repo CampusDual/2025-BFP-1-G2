a364a09468e44e2cd2d705e348a2fa85d43ef082
Victor Lopez Garcia <victlopgar@gmail.com>
2025-06-09 12:50:59 +0200

    Componentes de offer inicializados

---
+++ src/main/resources/front/2025-BFP-1-G2_FRONT/src/app/offer-panel/offer-table/offer-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-offer-table',
+  templateUrl: './offer-table.component.html',
+  styleUrls: ['./offer-table.component.css']
+})
+export class OfferTableComponent {
+
+}
