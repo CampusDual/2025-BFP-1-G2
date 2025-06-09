b9c2176d7faac29274fa30bd8a1d375952225fe7
EnmaLlDev <enmanuel.lledo.1998@gmail.com>
2025-06-09 14:19:07 +0200

    offerCard

--- src/main/resources/front/2025-BFP-1-G2_FRONT/src/app/app-routing.module.ts
+++ src/main/resources/front/2025-BFP-1-G2_FRONT/src/app/app-routing.module.ts
@@ -4,15 +4,18 @@ import { RegisterComponent } from './auth/register/register.component';
 import { LoginComponent } from './auth/login/login.component';
 import {CompanyPanelComponent} from "./company-panel/company-panel.component";
 import {AuthGuard} from "./auth/auth.guard";
+import { OfferCardComponent } from './offer-panel/offer-card/offer-card.component';
 
 
 //canActivate
 const routes: Routes = [
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+    { path: 'offer', loadChildren: () => import('./offer-panel/offer-panel.module').then(m => m.OfferPanelModule) },
   { path: '', redirectTo: 'register', pathMatch: 'full' },
   { path: 'register',  component: RegisterComponent },
   { path: 'login',  component: LoginComponent },
-  { path: 'company/:companyName', component: CompanyPanelComponent, canActivate: [AuthGuard] }
+  { path: 'company/:companyName', component: CompanyPanelComponent, canActivate: [AuthGuard] },
+  { path: 'card', component: OfferCardComponent}
 ];
 
 @NgModule({
