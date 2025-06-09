1bf28fb7c58d0508cc1ad8286405331b665d9827
EnmaLlDev <enmanuel.lledo.1998@gmail.com>
2025-06-09 14:19:07 +0200

    offerCard

--- src/main/resources/front/2025-BFP-1-G2_FRONT/src/app/offer-panel/offer-panel.module.ts
+++ src/main/resources/front/2025-BFP-1-G2_FRONT/src/app/offer-panel/offer-panel.module.ts
@@ -4,16 +4,23 @@ import { CommonModule } from '@angular/common';
 import { OfferPanelRoutingModule } from './offer-panel-routing.module';
 import { OfferTableComponent } from './offer-table/offer-table.component';
 import { OfferCardComponent } from './offer-card/offer-card.component';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    OfferTableComponent,
     OfferCardComponent
   ],
   imports: [
     CommonModule,
-    OfferPanelRoutingModule
+    MatCardModule,
+    MatButtonModule
+  ],
+  exports: [
+    OfferCardComponent
   ]
 })
-export class OfferPanelModule { }
+export class OfferPanelModule {
+
+ }
+
