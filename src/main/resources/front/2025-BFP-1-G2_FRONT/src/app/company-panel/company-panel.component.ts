5feac65dc5011c220897bffbb3e08f1f9ef186ed
Pablo <pablobarreiro131@gmail.com>
2025-06-09 11:31:38 +0200

    Formulario de ofertas actualizado.

--- src/main/resources/front/2025-BFP-1-G2_FRONT/src/app/company-panel/company-panel.component.ts
+++ src/main/resources/front/2025-BFP-1-G2_FRONT/src/app/company-panel/company-panel.component.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import { OfferService } from '../services/offer.service';
 
+
+interface Offer {
+  title: string;
+  description: string;
+}
 
 @Component({
   selector: 'app-company-panel',
@@ -20,22 +26,52 @@ export class CompanyPanelComponent implements OnInit {
 
 
 
-  constructor(private route: ActivatedRoute, private fb: FormBuilder) {}
+  constructor(private route: ActivatedRoute, private fb: FormBuilder, private offerService: OfferService) {}
 
   ngOnInit(): void {
     this.companyName = this.route.snapshot.paramMap.get('companyName');
   }
 
-  onSubmit(): void{
-    if (this.offersForm.valid) {
-      const offerData = this.offersForm.value;
-      console.log('Offer submitted:', offerData);
-    } else {
-      console.error('Form is invalid');
-    }
+onSubmit(): void {
+  if (!this.companyName) {
+    console.error('Company name is missing');
+    return;
   }
 
-  onReset(): void{
-    this.offersForm.reset();
+  if (this.offersForm.valid) {
+
+    const formValue = this.offersForm.value;
+    const title = formValue.title ?? '';
+    const description = formValue.description ?? '';
+
+    if (!title || !description) {
+      console.error('Title or Description are empty');
+      return;
+    }
+
+    const offerData: Offer = {
+      title,
+      description
+    };
+
+    this.offerService.createOffer(offerData, this.companyName).subscribe({
+      next: response => {
+        console.log('Offer created successfully', response);
+        this.offersForm.reset();
+      },
+      error: error => {
+        console.error('Error creating offer', error);
+      }
+    });
+
+  } else {
+    console.error('Form is invalid');
+    this.offersForm.markAllAsTouched();
   }
+}
+
+onReset(): void {
+  this.offersForm.reset();
+}
+
 }
