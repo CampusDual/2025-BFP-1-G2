d64f3297a271228ce85d8a088fcbc5952a5568a7
VictorLopezGarcia <victor.lopezg@udc.es>
2025-06-09 12:50:59 +0200

    Componentes de offer inicializados

--- src/main/resources/front/2025-BFP-1-G2_FRONT/src/app/offer-panel/offer-panel.module.ts
+++ src/main/resources/front/2025-BFP-1-G2_FRONT/src/app/offer-panel/offer-panel.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { OfferPanelRoutingModule } from './offer-panel-routing.module';
+import { OfferTableComponent } from './offer-table/offer-table.component';
+import { OfferCardComponent } from './offer-card/offer-card.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    OfferTableComponent,
+    OfferCardComponent
+  ],
   imports: [
     CommonModule,
     OfferPanelRoutingModule
