2c23cd32d4b5e3fca740974f4184a480e0c86a97
EnmaLlDev <enmanuel.lledo.1998@gmail.com>
2025-06-14 13:29:31 +0200

    implementation of offer details

--- 2025-BFP-1-G2_FRONT/src/app/offer-panel/offer-card/offer-card.component.ts
+++ 2025-BFP-1-G2_FRONT/src/app/offer-panel/offer-card/offer-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, HostListener, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-offer-card',
@@ -6,14 +6,39 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./offer-card.component.css']
 })
 export class OfferCardComponent {
-
   @Input() offer: any;
+  @Input() isSelected: boolean = false;
+
   isDisabled: boolean = true;
-  
-  constructor() {}
-toggleActions() {
-  this.isDisabled = !this.isDisabled;
-}
 
+  constructor(private elementRef: ElementRef) {}
 
+  @HostListener('document:click', ['$event'])
+  clickOutside(event: Event) {
+    if (!this.isDisabled && !this.elementRef.nativeElement.contains(event.target)) {
+      this.closeCard();
+    }
+  }
+
+  @HostListener('document:keydown.escape')
+  onEscapePress() {
+    if (!this.isDisabled) {
+      this.closeCard();
+    }
+  }
+
+  toggleSelection(event: Event) {
+    event.stopPropagation();
+    this.isSelected = !this.isSelected;
+  }
+
+  toggleActions(event: Event) {
+    event.stopPropagation();
+    this.isDisabled = !this.isDisabled;
+  }
+
+  closeCard() {
+    this.isDisabled = true;
+    this.isSelected = false;
+  }
 }
