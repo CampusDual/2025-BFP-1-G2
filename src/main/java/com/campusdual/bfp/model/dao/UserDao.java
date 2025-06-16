3ff1aff8c406567291dd4f0cc74814575f49dbb3
Pablo <pablobarreiro131@gmail.com>
2025-06-16 10:50:15 +0200

    BFP2025G2-10

--- src/main/java/com/campusdual/bfp/model/dao/UserDao.java
+++ src/main/java/com/campusdual/bfp/model/dao/UserDao.java
@@ -2,6 +2,7 @@ package com.campusdual.bfp.model.dao;
 
 import com.campusdual.bfp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
