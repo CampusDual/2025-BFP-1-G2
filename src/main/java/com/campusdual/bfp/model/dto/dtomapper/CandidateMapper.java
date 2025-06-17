3180e64403731e82aa73b2e6131553e40e48d8a4
Victor Lopez Garcia <victlopgar@gmail.com>
2025-06-17 12:00:11 +0200

    llamadas a api de lista de candidatos

--- src/main/java/com/campusdual/bfp/model/dto/dtomapper/CandidateMapper.java
+++ src/main/java/com/campusdual/bfp/model/dto/dtomapper/CandidateMapper.java
@@ -3,6 +3,7 @@ package com.campusdual.bfp.model.dto.dtomapper;
 import com.campusdual.bfp.model.Candidate;
 import com.campusdual.bfp.model.dto.CandidateDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
@@ -10,9 +11,16 @@ import java.util.List;
 public interface CandidateMapper {
     CandidateMapper INSTANCE = org.mapstruct.factory.Mappers.getMapper(CandidateMapper.class);
 
+    @Mapping(source = "user.login", target = "login")
+    @Mapping(source = "user.password", target = "password")
+    @Mapping(source = "user.email", target = "email")
+    @Mapping(source = "phoneNumber", target = "phoneNumber")
     CandidateDTO toDTO(Candidate candidate);
 
     List<CandidateDTO> toDTOList(List<Candidate> candidates);
 
+    @Mapping(target = "user.login", source = "login")
+    @Mapping(target = "user.password", source = "password")
+    @Mapping(target = "user.email", source = "email")
     Candidate toEntity(CandidateDTO candidateDTO);
 }
