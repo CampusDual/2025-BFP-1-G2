aab3c9aefe6749df506c9ae084f1cb27c1dc4a17
Victor Lopez Garcia <victlopgar@gmail.com>
2025-06-18 12:22:41 +0200

    didebar nav

--- 2025-BFP-1-G2_FRONT/src/app/side-nav/side-nav.component.ts
+++ 2025-BFP-1-G2_FRONT/src/app/side-nav/side-nav.component.ts
@@ -6,25 +6,10 @@ import { Router } from '@angular/router';
   templateUrl: './side-nav.component.html',
   styleUrls: ['./side-nav.component.css']
 })
-export class SideNavComponent implements OnInit {
-  shouldRun = false;
+export class SideNavComponent{
   showFiller = false;
-  isCandidate = false;
-  isCompany = false;
-  isAuthenticated = false;
 
   constructor(private router: Router) { }
-
-  ngOnInit(): void {
-    // Aquí deberías obtener el estado de autenticación y el rol del usuario
-    this.checkAuthStatus();
-  }
-
-  private checkAuthStatus(): void {
-    this.isAuthenticated = true; // Ejemplo
-    this.isCandidate = true; // o this.isCompany = true;
-    this.shouldRun = this.isAuthenticated;
-  }
 
   navigateTo(route: string): void {
     this.router.navigate([route]);
