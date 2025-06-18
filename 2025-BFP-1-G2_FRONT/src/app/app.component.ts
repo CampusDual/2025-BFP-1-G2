ca0b9591ef297ce4d6484bcc42b0852bec6561e8
Victor Lopez Garcia <victlopgar@gmail.com>
2025-06-18 13:12:06 +0200

    sidebar nav

--- 2025-BFP-1-G2_FRONT/src/app/app.component.ts
+++ 2025-BFP-1-G2_FRONT/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import {Component} from '@angular/core';
-import {animate, style, transition, trigger} from "@angular/animations";
-import {RouterOutlet} from "@angular/router";
+import { Component } from '@angular/core';
+import { animate, style, transition, trigger } from "@angular/animations";
+import { RouterOutlet } from "@angular/router";
+import { AuthService } from './auth/services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -10,16 +11,37 @@ import {RouterOutlet} from "@angular/router";
     trigger('fadeAnimation', [
       transition('* <=> *', [
         style({ opacity: 0, transform: 'translateY(20px)', filter: 'blur(2px)' }),
-        animate('400ms', style({ opacity: 1 , transform: 'translateY(0px)', filter: 'blur(0px)' })),
+        animate('400ms', style({ opacity: 1, transform: 'translateY(0px)', filter: 'blur(0px)' })),
       ])
     ]),
   ]
-} )
+})
 
 export class AppComponent {
-    showFiller = false;
+  showFiller = false;
+  isCompany = false;
+  isCandidate = false;
 
-  getRouterOutletState(outlet: RouterOutlet) {
+  constructor (protected authService: AuthService) {
+    this.loadUserRole();
+  }
+
+  loadUserRole() {
+    this.authService.hasRole('ROLE_COMPANY').subscribe({
+      next: (hasRole) => {
+        this.isCompany = hasRole;
+      }
+    });
+
+    this.authService.hasRole('ROLE_CANDIDATE').subscribe({
+      next: (hasRole) => {
+        this.isCandidate = hasRole;
+      }
+    });
+  }
+
+  getRouterOutletState(outlet: RouterOutlet)
+   {
     return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
   }
 }
